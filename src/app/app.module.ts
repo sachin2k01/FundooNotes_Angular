b1b2bda0cf04bc7e70afd96042e89d149849d134
sachin2k01 <thenamesachin@gmail.com>
2024-02-15 12:56:16 +0530

    Register page is started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent 
+    LoginComponent,
+    RegisterComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
